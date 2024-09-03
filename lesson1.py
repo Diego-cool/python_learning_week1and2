b1863ed99d6aed492bb4522bb8afafd657abf13e
Venegas <dvenegas7@cps.k12.il.us>
2024-09-03 12:46:35 -0500

    try it one more tiem

---
+++ lesson1.py
@@ -0,0 +1,24 @@
+# print("hello my name is Diego")
+#this is the print statement 
+#print statement allows us to print out a statement 
+#comments are not executed by the computer  
+#comments are used to help us understand the code 
+# print("I am a student at john hancock college prep")
+# print("I am learning python")
+# create input for the user 
+name = input ("what is your name?")
+print("hello", name)
+#ask for user for age
+#print out the age 
+#ask the user for their favorite color 
+#print out the favorite color 
+#ask the user for their favorite food 
+#print out the favorite food
+age = input("what is your age")
+print(name,"is", age, "years old")
+color=input("what is your favorite color")
+print("I love",color, "too") 
+food=input("what is your favorite food")
+print( food, "sounds yummy to bad im a robot and cant eat")
+favorite_animal=input("what is your favorite animal?")
+print("your favorite animal is a"+favorite_animal)
